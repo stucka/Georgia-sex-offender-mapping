149b5a7c47a7d03dae9887d517d73462892bccb1
Mike Stucka <stucka@whitedoggies.com>
2011-06-11 23:24:28 -0400

    Big optimization; implemented warning_flag

--- scrape.py
+++ scrape.py
@@ -45,7 +45,7 @@ headerrow = ['name','sex','race','yob','height','weight','haircolor',
 'eyecolor','scarsmarkstattoos','streetnumber','street','city','state',
 'zip','countyraw','registrationdate','crime','convictiondate','convictionstate',
 'incarcerated','predator','absconder','id','resverificationdate',
-'glat','glong','latlong','imgurl','ageish','countynamefix']
+'glat','glong','latlong','imgurl','ageish','countynamefix', 'warning_flag']
 outputcsv.writerow(headerrow)
 
 
@@ -96,23 +96,7 @@ for line in localcsv:
     for idx, val in enumerate(line):
         line[idx] = val.title().strip();
 
-    line[7] = colornamefix(line[7])   #Fix eye color
-    line[6] = colornamefix(line[6])   #Fix hair color
-    age = int(time.strftime('%Y')) - int(line[3])
-    line[12] = line[12].upper()         #Fix one state
-    line[18] = line[18].upper()         #Fix the other state
-    line[1] = line[1].lower()           #lowercase sex
-    line[2] = line[2].lower()           #lowercase race
-    line[22] = line[22].lower()         #lowercase ID (important!)
-    if len(line[14]) > 2:
-        CountyTextFix = line[14] + " County, "
-        CountyNameFix = line[14] + " County"
-    else:
-        CountyTextFix = ''
-        CountyNameFix = ''
-
-    fulladdy = line[9] + " " + line[10] + ", " + line[11] + ", " + CountyTextFix + line[12] + " " + line[13]
-    
+    line[12] = line[12].upper()         #Fix one state    
 
 
 ## Is this in a place we actually care about?
@@ -120,6 +104,40 @@ for line in localcsv:
 #        print "OK, I'm ignoring ", line[0], ": at",fulladdy
          pass
     else:
+        line[7] = colornamefix(line[7])   #Fix eye color
+        line[6] = colornamefix(line[6])   #Fix hair color
+        age = int(time.strftime('%Y')) - int(line[3])
+        line[18] = line[18].upper()         #Fix the other state
+        line[1] = line[1].lower()           #lowercase sex
+        line[2] = line[2].lower()           #lowercase race
+        line[22] = line[22].lower()         #lowercase ID (important!)
+        if line[20] == 'Predator':
+            warning_flag = "*Predator*"
+        else:
+            warning_flag = ''
+        if line[21] == 'Absonconder':
+            warning_flag = warning_flag + " *Absconded*"
+        else:
+            pass
+        if line[19] == 'Incarcerated':
+            warning_flag = warning_flag + " *Incarcerated*"
+        else:
+            pass
+
+        warning_flag = warning_flag.strip()
+        print warning_flag
+#
+        if len(line[14]) > 2:
+            CountyTextFix = line[14] + " County, "
+            CountyNameFix = line[14] + " County"
+        else:
+            CountyTextFix = ''
+            CountyNameFix = ''
+
+        fulladdy = line[9] + " " + line[10] + ", " + line[11] + ", " + CountyTextFix + line[12] + " " + line[13]
+
+
+
         
 ##
 ## OK, so let's see if we already know where this address is. We can
@@ -175,6 +193,7 @@ for line in localcsv:
         line.append('http://services.georgia.gov/gbi/sorpics/' + line[22] + '.jpg')
         line.append(age)
         line.append(CountyNameFix)
+        line.append(warning_flag)
     #    latlong = glat + ", " + glong
     #    imgurl = 'http://services.georgia.gov/gbi/sorpics/' + line[22] + '.jpg'
     #    outputcsv.writerow(line, glat, glong, latlong, imgurl))
@@ -187,14 +206,21 @@ for line in localcsv:
 
 """
 To-do:
-Start exporting data out to CSV
 Integrate with Google Fusion tables
-Download photos
-Allow parsing based on county name
 Split first and last names
 Add in age, first, last
 Fix dates?
-Figure out how to handle multiple addresses
+Split height into feet (height:1) and inches
+str(value(height(-2:)
+Do registration, convicted, resverification dates go into Fusion Tables right?
+Is there a fuzzy time module, like "a few months ago"?
+Figure out how to handle multiple addresses -- JavaScript or in-house?
+How handle incarcerated/predator/absconded?
+warning_flag text string= ''
+
+Then add warning_flag to header, CSV output
+
+
 Cure cancer
 """ 
 
@@ -239,5 +265,6 @@ And then our new stuff
 27 imgurl
 28 age (approximate)
 29 CountyNameFix
+30 warning_flag (predator-obsconder-incarcerated)
 
 """
